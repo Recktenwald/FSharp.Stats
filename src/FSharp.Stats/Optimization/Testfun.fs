15c1fad04bf5ed4b80a86ff1407bc7b525d560a6
Rene Recktenwald <rene.recktenwald@posteo.de>
2022-04-30 12:34:23 +0200

    Add various test functions for optimization.

---
+++ src/FSharp.Stats/Optimization/Testfun.fs
@@ -0,0 +1,39 @@
+namespace FSharp.Stats.Optimization
+
+module Testfun = 
+    open FSharp.Stats
+    // Unbounded Optimization
+    // // Rosenbrock function
+    let rosenbrock v = 
+        let n = Vector.length v 
+        [0..(n-2)]
+        |> List.map (fun i -> 100.0*( (v[i+1] - v[i]*v[i])**2.0) + ((1.0 - v[i])**2.0))
+        |> List.sum 
+
+    // // Sphere function
+    let sphere v = 
+        Vector.dot v v
+
+    // Bounded Optimization 
+
+    // // Ackley function 
+    let ackley v = 
+        let n = float (Vector.length v)
+        let firstSummand = 
+            20.0*exp(-0.2*Math.Sqrt((sphere v)/n))
+
+        let sumOfCos = 
+            v
+            |> Vector.map (fun vi -> Math.Cos(2.0*Math.PI*vi))
+            |> Vector.sum 
+        let secondSummand = 
+            exp(sumOfCos/n)
+        
+        - firstSummand - secondSummand + 20.0 + exp(1.0)
+
+    // //  Himmelblau function
+    /// This is a 2D -> 1D function.
+    /// Only uses the first two entries if a longer vector is passed as an argument.
+    let himmelblau v = 
+        (v[0]**2.0 + v[1] - 11.0)**2.0 + (v[0] + v[1]**2.0 - 7)**2.0
+    
